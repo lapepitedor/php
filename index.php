75934912f8c377c218eaeddf53fa33c97dc60532
lapepitedor <messubrinda@gmail.com>
2024-12-04 16:28:55 +0100

    variable and data types

--- index.php
+++ index.php
@@ -6,7 +6,25 @@
     <title>Document</title>
 </head>
 <body>
-   $name = "Brinda";
-   echo $name
+  
+   <?php
+   //scalar types
+   $string = "";
+   $int = 12345;
+   $float = 2.433;
+   $bool = false;
+   $name = "brinda";
+
+   //Array type
+$array = array("Daniel", "Bell", "TRES");
+$names = ["Daniel", "Bell", "TRES"];
+
+ //Object type
+ //$object = new Car();
+
+
+
+   ?>
+   <p> My name is <?php echo $name?></p>
 </body>
 </html>
